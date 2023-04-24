f51afd88448c5e83548a9f9cdb0ad15906c89fcd
Zachary Young <zhyoung@iu.edu>
2023-04-24 17:58:39 -0400

    final push

--- src/main/java/gatewayserver/GatewayserverApplication.java
+++ src/main/java/gatewayserver/GatewayserverApplication.java
@@ -2,9 +2,9 @@ package gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
